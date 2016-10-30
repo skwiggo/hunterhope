625f9187e8a2573936555954c9044674c605f294
James Hardy <skwiggo123@gmail.com>
2016-10-30 14:58:15 +0000

    routes testing

--- angular/client/build/hunterHope.js
+++ angular/client/build/hunterHope.js
@@ -1,6 +1,6 @@
-angular.module('hunterHope', ['ui.router']) 
+var myApp = angular.module('hunterHope', ['ui.router']) 
 
-.config([
+myApp.config([
 '$stateProvider',
 '$urlRouterProvider',
 function($stateProvider, $urlRouterProvider) {
@@ -12,13 +12,43 @@ function($stateProvider, $urlRouterProvider) {
       controller: 'BlogCtrl'
     });
 
+  $stateProvider
+    .state('music', {
+      url: '/music',
+      templateUrl: '/music.html',
+      controller: 'MusicCtrl'
+    });
+
+  $stateProvider
+    .state('tour', {
+      url: '/tour',
+      templateUrl: '/tour.html',
+      controller: 'TourCtrl'
+    });
+
   $urlRouterProvider.otherwise('blog');
 }])
 
-.controller('BlogCtrl', ["$scope", "$http", function($scope, $http) {
+myApp.controller('BlogCtrl', ["$scope", "$http", function($scope, $http) {
     $http.get("http://localhost:5000/blogposts")
     .then(function(response) {
         $scope.posts = response.data;
+    });
+  }  
+]);
+
+myApp.controller('MusicCtrl', ["$scope", "$http", function($scope, $http) {
+    $http.get("http://localhost:5000/songs")
+    .then(function(response) {
+        $scope.songs = response.data;
+    });
+  }  
+]);
+
+myApp.controller('TourCtrl', ["$scope", "$http", function($scope, $http) {
+    $http.get("http://localhost:5000/tourdates")
+    .then(function(response) {
+        $scope.tourDates = response.data;
     });
   }  
 ]);
