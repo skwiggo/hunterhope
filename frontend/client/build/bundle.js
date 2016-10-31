acb971904de1842c88ae66a62789843f9d222102
James Hardy <skwiggo123@gmail.com>
2016-10-31 21:08:57 +0000

    added markers to tourdates

--- frontend/client/build/bundle.js
+++ frontend/client/build/bundle.js
@@ -36485,9 +36485,19 @@
 	      $http.get("http://localhost:5000/tourdates")
 	      .then(function(response) {
 	          $scope.tourDates = response.data;
-	      });
-	    }  
+	      })
+	      $scope.map = function(NgMap) {
+	        NgMap.getMap().then(function(map) {
+	        console.log(map.getCenter());
+	        console.log('markers', map.markers);
+	        console.log('shapes', map.shapes);
+	        })
+	      }   
+	    }
 	  ]);
+	
+	
+	
 	
 
 
--- frontend/client/src/tourcontroller.js
+++ frontend/client/src/tourcontroller.js
@@ -2,7 +2,17 @@ angular.module('hunterHope').controller('TourCtrl', ["$scope", "$http", function
       $http.get("http://localhost:5000/tourdates")
       .then(function(response) {
           $scope.tourDates = response.data;
-      });
-    }  
+      })
+      $scope.map = function(NgMap) {
+        NgMap.getMap().then(function(map) {
+        console.log(map.getCenter());
+        console.log('markers', map.markers);
+        console.log('shapes', map.shapes);
+        })
+      }   
+    }
   ]);
+
+
+
 
