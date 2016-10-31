537ee33c7df18306e03d4b82a32da4054db90c41
James Hardy <skwiggo123@gmail.com>
2016-10-31 00:40:46 +0000

    more directory changes - look into webpack compatability

--- angular/server.js
+++ angular/server.js
@@ -6,8 +6,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-app.use(express.static('client/build'));
-
+app.use(express.static('/client/build'));
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
